65b800d730a726d6c618515e7505f01c71ecbf0a
LeaderDouglas <153232919+LeaderDouglas@users.noreply.github.com>
2025-03-23 16:05:26 -0700

    Update home.tsx
    Firestone Added

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,13 +1,44 @@
 import { HomeCategory, HomeTopBar, JobRecommendation } from "@/components/home";
 import Spacer from "@/components/spacer";
-import { Colors } from "@/constants/Colors";
 import { moderateScale } from "@/constants/responsive";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import {SafeAreaView} from "react-native";
+import auth from "@react-native-firebase/auth";
+import firestore from "@react-native-firebase/firestore";
 
 export default function HomeScreen() {
+  const [userDetails, setUserDetails] = useState<any>(null);
+  console.log("🚀 ~ HomeScreen ~ userDetails:", userDetails)
+
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      const user = auth().currentUser;
+
+      if (user) {
+        try {
+          const userDoc = await firestore().collection("users").doc(user.uid).get();
+
+          if (userDoc.exists) {
+            const userData = userDoc.data();
+            setUserDetails(userData);
+           
+          } else {
+            console.log("No such document!");
+          }
+        } catch (error) {
+          console.error("Error fetching user details: ", error);
+        }
+      } else {
+        console.log("No user is signed in.");
+      }
+    };
+
+    fetchUserDetails();
+  }, []);
+
   return (
     <SafeAreaView style={{ flex: 1, marginTop: moderateScale(40) }}>
-      <HomeTopBar />
+      <HomeTopBar name={userDetails?.name || ""}/>
       <Spacer />
       <HomeCategory />
       <Spacer />
